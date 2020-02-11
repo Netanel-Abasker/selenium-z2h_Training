c9f7f8517a0070d572e314bf4ac65885c1bea1c2
abaskn <Netanel.Abasker@emc.com>
2020-02-11 11:24:18 +0200

    add loginPage class

--- ui_builder/src/main/java/builder/pages/LoginPage.java
+++ ui_builder/src/main/java/builder/pages/LoginPage.java
@@ -5,14 +5,37 @@ import org.openqa.selenium.WebDriver;
 import builder.BasePage;
 import lombok.Getter;
 import lombok.Setter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 @Getter
 @Setter
 public class LoginPage extends BasePage {
 
-    //TODO:
+    @FindBy(id = "login_field")
+    private WebElement inputUserName;
+
+    @FindBy(id = "password")
+    private WebElement inputPassword;
+
+    @FindBy(name = "commit")
+    private WebElement commitButton;
 
     public LoginPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void click(){
+        commitButton.click();
+    }
+
+    public void inputUserName(String input){
+        inputUserName.clear();
+        inputUserName.sendKeys(input);
+    }
+
+    public void inputPassword(String input){
+        inputPassword.clear();
+        inputPassword.sendKeys(input);
     }
 }
