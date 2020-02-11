502495933500e1eab3ea2e64ad8fc38080aa7137
abaskn <Netanel.Abasker@emc.com>
2020-02-11 11:51:55 +0200

    add Login_Test_1 with login flow

--- testing/src/main/java/ui_integration/LoginTest_1.java
+++ testing/src/main/java/ui_integration/LoginTest_1.java
@@ -1,18 +1,25 @@
 package ui_integration;
 
+import builder.ChromeStandaloneDriver;
+import builder.flows.LoginFlow;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest_1 {
 
+    private String BASE_URL = "https://github.com/login";
+    private String USER_NAME = "";
+    private String PASSWORD = "";
+    private String EXPECTE_TITLE = "GitHub";
+
     @Test
     public void loginPositive() throws Exception {
-        /*
-            TODO:
-                1. Init Driver and start
-                2. Get Required URL
-                3. Run Login  FLow
-                4. Validate Success
-         */
+        ChromeStandaloneDriver chromeStandaloneDriver = new ChromeStandaloneDriver();
+        chromeStandaloneDriver.start();
+        chromeStandaloneDriver.getDriver().get(BASE_URL);
+        LoginFlow.basicLoginFluentFlow(chromeStandaloneDriver, USER_NAME, PASSWORD);
+        Assert.assertEquals(chromeStandaloneDriver.getDriver().getTitle(), EXPECTE_TITLE);
+        chromeStandaloneDriver.getDriver().close();
     }
 
 }
